e901e2dfdd710f3175fe51c74600dde174e34823
Tess <tniehoff24@gmail.com>
2020-11-28 21:03:54 -0500

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
      Weather App
+     <footer>  
+     This is coded by Tess Niehoff   
+       <a href="https://github.com/tniehoff24?tab=repositories" taret="_blank"> 
+       on Github
+     </a>
+     .
+     </footer>
+
     </div>
   );
 }
